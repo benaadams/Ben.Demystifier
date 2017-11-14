bd0c9067a0d5de3afc965bb498f9e087bcfe3559
Ben Adams <thundercat@illyriad.co.uk>
2017-11-14 02:54:28 +0000

    Add InheritenceTests

---
+++ test/Ben.Demystifier.Test/InheritenceTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ben.Demystifier.Test
+{
+    public class InheritenceTests
+    {
+        private abstract class BaseClass
+        {
+            public abstract Task<object> Method();
+        }
+
+        private class ImplClass : BaseClass
+        {
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            public override Task<object> Method()
+            {
+                throw new Exception();
+            }
+        }
+
+        [Fact]
+        public async Task ImplementedAbstractMethodDoesNotThrow()
+        {
+            // Arrange
+            var instance = new ImplClass();
+
+            // Act
+            Exception exception = null;
+            try
+            {
+                await instance.Method();
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+
+            // Act
+            var est = new EnhancedStackTrace(exception);
+        }
+    }
+}
