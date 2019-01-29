886ddea8ea7868fcea8c35755b70c468837b233d
Aristarkh Zagorodnikov <xm@drive.net>
2019-01-29 22:33:40 +0300

    More specific tests

--- test/Ben.Demystifier.Test/EnhancedStackTraceTests.cs
+++ test/Ben.Demystifier.Test/EnhancedStackTraceTests.cs
@@ -22,11 +22,17 @@ namespace Ben.Demystifier.Test
         }
 
         [Theory]
-        [InlineData(@"file://Sources\My 100%.Done+Solution\Foo`1.cs", @"/My 100%.Done+Solution/Foo`1.cs")]
-        [InlineData(@"d:\Public Files+50%.Done\Src\Foo`1.cs", @"d:/Public Files+50%.Done/Src/Foo`1.cs")]
-        public void SpecialPathCharactersAreHandledCorrectly(string original, string expected)
+        [InlineData(@"file://Sources\My 100%.Done+Solution\Foo`1.cs", @"/My 100%.Done+Solution/Foo`1.cs", false)]
+        [InlineData(@"d:\Public Files+50%.Done\Src\Foo`1.cs", @"d:/Public Files+50%.Done/Src/Foo`1.cs", false)]
+        [InlineData(@"\.\Public Files+50%.Done\Src\Foo`1.cs", @"/./Public Files+50%.Done/Src/Foo`1.cs", true)]
+        public void SpecialPathCharactersAreHandledCorrectly(string original, string expected, bool normalize)
         {
             var converted = EnhancedStackTrace.TryGetFullPath(original);
+            if (normalize)
+            {
+                converted = NormalizePath(converted);
+            }
+
             Assert.Equal(expected, converted);
         }
 
