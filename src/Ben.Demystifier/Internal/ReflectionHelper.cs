43c92b54e549c4f22a7c35244dbf756a1e421811
Martin Stühmer <3283596+samtrion@users.noreply.github.com>
2019-12-03 15:36:58 +0100

    Simplified GetPrefix `in` detection (#95)

--- src/Ben.Demystifier/Internal/ReflectionHelper.cs
+++ src/Ben.Demystifier/Internal/ReflectionHelper.cs
@@ -15,14 +15,6 @@ namespace System.Diagnostics.Internal
         private static PropertyInfo tranformerNamesLazyPropertyInfo;
 
         /// <summary>
-        /// Returns true if <paramref name="type"/> is <code>System.Runtime.CompilerServices.IsReadOnlyAttribute</code>.
-        /// </summary>
-        public static bool IsReadOnlyAttribute(this Type type)
-        {
-            return type.Namespace == "System.Runtime.CompilerServices" && type.Name == "IsReadOnlyAttribute";
-        }
-
-        /// <summary>
         /// Returns true if the <paramref name="type"/> is a value tuple type.
         /// </summary>
         public static bool IsValueTuple(this Type type)
