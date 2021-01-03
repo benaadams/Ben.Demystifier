7c10e03cadb4ce6b415943571d5914e5c1587bf4
Ben Adams <thundercat@illyriad.co.uk>
2021-01-03 21:28:59 +0000

    Add RecursionTests

---
+++ test/Ben.Demystifier.Test/RecursionTests.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ben.Demystifier.Test
+{
+    public class RecursionTests
+    {
+        [Fact]
+        public async Task DemystifiesAsyncRecursion()
+        {
+            Exception demystifiedException = null;
+
+            try
+            {
+                await RecurseAsync(10);
+            }
+            catch (Exception ex)
+            {
+                demystifiedException = ex.Demystify();
+            }
+
+            // Assert
+            var stackTrace = demystifiedException.ToString();
+            stackTrace = LineEndingsHelper.RemoveLineEndings(stackTrace);
+            var trace = string.Join("", stackTrace.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .ToArray());
+            var expected = string.Join("", new[] {
+                "   at async Task<int> Ben.Demystifier.Test.RecursionTests.RecurseAsync(int depth)",
+                "   at async Task<int> Ben.Demystifier.Test.RecursionTests.RecurseAsync(int depth) x 10",
+                "   at async Task Ben.Demystifier.Test.RecursionTests.DemystifiesAsyncRecursion()"
+            });
+            Assert.Equal(expected, trace);
+        }
+
+        [Fact]
+        public void DemystifiesRecursion()
+        {
+            Exception demystifiedException = null;
+
+            try
+            {
+                Recurse(10);
+            }
+            catch (Exception ex)
+            {
+                demystifiedException = ex.Demystify();
+            }
+
+            // Assert
+            var stackTrace = demystifiedException.ToString();
+            stackTrace = LineEndingsHelper.RemoveLineEndings(stackTrace);
+            var trace = string.Join("", stackTrace.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .ToArray());
+            var expected = string.Join("", new[] {
+                "   at int Ben.Demystifier.Test.RecursionTests.Recurse(int depth)",
+                "   at int Ben.Demystifier.Test.RecursionTests.Recurse(int depth) x 10",
+                "   at void Ben.Demystifier.Test.RecursionTests.DemystifiesRecursion()"
+            });
+            Assert.Equal(expected, trace);
+        }
+
+        async Task<int> RecurseAsync(int depth)
+        {
+            if (depth > 0) await RecurseAsync(depth - 1);
+            throw new InvalidOperationException();
+        }
+
+        int Recurse(int depth)
+        {
+            if (depth > 0) Recurse(depth - 1);
+            throw new InvalidOperationException();
+        }
+    }
+}
