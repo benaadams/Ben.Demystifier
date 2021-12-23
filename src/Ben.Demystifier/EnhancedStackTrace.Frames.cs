0bf21cbf607a932f350ee77207b1d1e9d9468b68
Simon Cropp <simon.cropp@gmail.com>
2021-12-23 17:03:20 +1100

    only get field value when is delegate

--- src/Ben.Demystifier/EnhancedStackTrace.Frames.cs
+++ src/Ben.Demystifier/EnhancedStackTrace.Frames.cs
@@ -188,16 +188,19 @@ namespace System.Diagnostics
                             var fields = type.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
                             foreach (var field in fields)
                             {
-                                var value = field.GetValue(field);
-                                if (value is Delegate d && d.Target is not null)
+                                if (typeof(Delegate).IsAssignableFrom(field.FieldType))
                                 {
-                                    if (ReferenceEquals(d.Method, originMethod) &&
-                                        d.Target.ToString() == originMethod.DeclaringType?.ToString())
+                                    var value = field.GetValue(field);
+                                    if (value is Delegate d && d.Target is not null)
                                     {
-                                        methodDisplayInfo.Name = field.Name;
-                                        methodDisplayInfo.IsLambda = false;
-                                        method = originMethod;
-                                        break;
+                                        if (ReferenceEquals(d.Method, originMethod) &&
+                                            d.Target.ToString() == originMethod.DeclaringType?.ToString())
+                                        {
+                                            methodDisplayInfo.Name = field.Name;
+                                            methodDisplayInfo.IsLambda = false;
+                                            method = originMethod;
+                                            break;
+                                        }
                                     }
                                 }
                             }
